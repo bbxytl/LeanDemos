5d9b3d71acbbab2369325209da435affbae52f70
bbxytl <bbxytl@gmail.com>
2014-12-17 16:23:51 +0800

    update1.0

---
+++ GridppReport/GridppReport.pro
@@ -0,0 +1,22 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-12-16T15:09:10
+#
+#-------------------------------------------------
+
+QT       += core gui axcontainer
+CONFIG += axcontainer
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = GridppReport
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h \
+    ahead.h
+
+FORMS    += mainwindow.ui
---
+++ GridppReport/ahead.h
@@ -0,0 +1,39 @@
+#ifndef AHEAD_H
+#define AHEAD_H
+
+
+#include <QString>
+#include <QStringList>
+#include <QAxWidget>
+#include <QAxObject>
+
+#include <QDebug>
+
+
+#define GridppReport            "{13c28ad0-f195-4319-b7d7-a1bdaa329fb8}"
+#define GRDisplayViewer         "{613f3ae0-d4eb-4f9f-b750-576c09700351}"
+#define GRDisplayViewerProps    "{9d74b4b2-7e9b-48ed-be28-d3531c2df170}"
+#define GRPrintViewer           "{e497f563-e14c-48ec-87fa-e9c514d3bc0c}"
+#define GRPrintViewerProps      "{ca1fa286-552a-4299-a3d8-89593eccb553}"
+
+
+
+
+#define _IGridppReportEvents    "{330f80f5-4568-4f70-bfcb-683d3b90febb}"
+#define _IGRDisplayViewerEvents "{2564dce8-fedb-4eb6-b7f9-5026f7f1041e}"
+
+
+#define IGRDisplayViewer        "{fba070ab-385f-4988-b7a1-ce6b72799b48}"
+#define IGRPrintViewer          "{0574abeb-b0da-465d-9eb7-286c00f3628f}"
+
+#define IGRGroup                "{f803ae1d-b578-490a-a1fe-38976ad2b625}"
+#define IGRObject               "{07120975-b963-4f85-9b4d-0ac979febb5d}"
+#define IGRGroupHeader          "{5c2fc87f-12f8-4a09-8a4a-792671c03c74}"
+#define IGRGroupSection         "{36971202-d715-4afc-83d4-7c0ddd8872e8}"
+#define IGRSection              "{77fd1006-7067-41ff-a712-0f356a6ace8c}"
+#define IGRFont                 "{d4708b2f-dc00-4ef6-9559-a5dd9e86047c}"
+#define IGRControls             "{48636e6a-758e-46c0-b37a-c2a2cac9469a}"
+#define IGRControl              "{07120975-b963-4f75-9b4d-0ac979febb5d}"
+
+
+#endif // AHEAD_H
